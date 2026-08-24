a2c451e1fe1b03e33cb1926a11fd4cd26609cfa0
Nagesh <nagesh3.13@gmail.com>
2026-08-24 10:01:33 +0530

    Tribonacci problem

--- dsa/dp/Tribonacci.java
+++ dsa/dp/Tribonacci.java
@@ -1,4 +1,32 @@
 package dsa.dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tribonacci {
+
+    public static void main(String[] args) {
+        System.out.println(output(7, new HashMap<>()));
+        System.out.println(output(9, new HashMap<>()));
+        System.out.println(output(31, new HashMap<>()));
+    }
+
+    private static int output(int n, Map<Integer, Integer> memo) {
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        if (n == 0 || n < 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        int output = output(n - 1, memo) + output(n - 2, memo) + output(n - 3, memo);
+        memo.put(n, output);
+        return output;
+
+    }
 }
